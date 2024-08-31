0db1e8a58059a1e861a373ce8e64e6215c8846ab
8d4c969697c18e71 <tk.04256800@gmail.com>
2024-08-31 21:00:32 +0900

    last

--- letters.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("img").imagesLoaded(function () {
-    $(".thumnail-right-box img").on("click", function () {
-      var img_src = $(this).attr("src");
-      $(".thumnail-left-box img").attr("src", img_src);
-    });
-  });
-});
-
